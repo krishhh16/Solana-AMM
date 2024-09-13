2b30c6fc4228b72de8c10e8620f10b9bf0da0ec6
krishhh16 <krishh16@yahoo.com>
2024-09-13 18:32:30 +0530

    added sol to wsol conversion implementation in pool intialization instruction

--- lp-program/programs/lp-program/src/lib.rs
+++ lp-program/programs/lp-program/src/lib.rs
@@ -1,14 +1,41 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Token, TokenAccount, Mint, Transfer};
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+use anchor_lang::system_program;
 
-declare_id!("6yjwTv7GfaxmCQkW8xZEfhFmp43h5v5cNs2B3TtFCFqq");
+declare_id!("CFqZq4eVNHpjntzo1vF2fjUF1VwSGn81SoEngv559syq");
 
 #[program]
 pub mod lp_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>,pool_id:u64) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>,pool_id:u64, sol_amount: u64) -> Result<()> {
+
+        let pool = &mut ctx.accounts.pool;
+        
+        // Converting the sol transfered to the program into wsol so that we don't have to handle the sol and token seperately
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer {
+                    from: ctx.accounts.creator.to_account_info(),
+                    to: ctx.accounts.wsol_vault.to_account_info()
+                }
+            ),
+            sol_amount
+        );
+
+        token::mint_to(
+            CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), token::MintTo{
+                mint: ctx.accounts.wsol_mint.to_account_info(),
+                to: ctx.accounts.user_wsol_account.to_account_info(),
+                authority: ctx.accounts.wsol_vault.to_account_info()
+            } , &[&[b"wsol_vault", &[ctx.bumps.wsol_vault]]],
+        ),
+            sol_amount
+        );
+
+        
+
         Ok(())
     }
 }
@@ -24,11 +51,16 @@ pub struct Initialize<'info> {
         bump
     )]
     pub pool: Account<'info, Pool>,
+    #[account(
+        mut, 
+        seeds = [b"wsol_valut"],
+        bump
+    )]
+    pub wsol_vault: SystemAccount<'info>,
+    pub wsol_mint: Account<'info, Mint>,
     #[account(mut)]
-    pub token_a_reserve: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub token_b_reserve: Account<'info, TokenAccount>,
-    
+    pub user_wsol_account: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     #[account(mut)]
     pub creator: Signer<'info>
